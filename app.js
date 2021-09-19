85e930918ee83d2d0cb081bb148b3cc3c6e539f1
Juan Manuel Flecha <flechajm@gmail.com>
2021-09-19 19:12:17 -0300

    Add favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./router/router");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const Middleware = new (require("./middleware/middleware"))();
 
@@ -9,6 +9,6 @@ app.set("/favicon.ico", express.static("favicon.ico"));
 app.use("/", router);
 app.use(Middleware.validateApiKey);
 
-app.listen(port, () => {
-  console.log("App is running on port " + port);
+app.listen(PORT, () => {
+  console.log("App is running on port " + PORT);
 });
