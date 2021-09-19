76f5e688305a7062e72e34f93ae244ec776e3f2c
Juan Manuel Flecha <flechajm@gmail.com>
2021-09-19 19:34:37 -0300

    Fix favicon.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const Middleware = new (require("./middleware/middleware"))();
 
-app.set("/favicon.ico", express.static("favicon.ico"));
+app.use("/favicon.ico", express.static("favicon.ico"));
 app.use("/", router);
 app.use(Middleware.validateApiKey);
 
