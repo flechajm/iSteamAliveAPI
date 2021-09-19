8e4f0f704dac594c7eab50f455c0daf9d9d97151
Juan Manuel Flecha <flechajm@gmail.com>
2021-09-19 18:42:17 -0300

    Add a separate module that represents the JSON structure.

---
+++ scrapper/status.js
@@ -0,0 +1,24 @@
+const status = {
+  steam_platform: {
+    online: "",
+    ingame: "",
+    store: "",
+    community: "",
+    webapi: "",
+    connection_managers: "",
+  },
+  game_coordinators: {
+    team_fortress2: "",
+    dota2: "",
+    artifact: "",
+    underlords: "",
+    csgo: "",
+  },
+  csgo_services: {
+    sessions_logon: "",
+    player_inventories: "",
+    matchmaking_scheduler: "",
+  },
+};
+
+module.exports = status;
